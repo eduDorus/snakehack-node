503049d36a4f53d44e5d116f849987c5a86b33c4
Dorus Janssens <dorus.janssens@stud.hslu.ch>
2017-11-11 15:09:24 +0100

    Update nearest food

--- routes/index.js
+++ routes/index.js
@@ -29,39 +29,49 @@ router.post('/move', function (req, res) {
   console.log(JSON.stringify(req.body, null, 2));
   const width = req.body.width;
   const height = req.body.height;
+  const food = req.body.food;
 
   // Create Gameboard
   let gameboard = createEmptyGameBoard(width, height, 0);
   gameboard = setSnakesOnGameboard(gameboard, req.body.snakes);
   //gameboard = setFoodOnGameboard(gameboard, req.body.food);
 
+  // Get snake hack
   const head = getSnakeHead(req.body.you, req.body.snakes);
   const headX = head[0]
   const headY = head[1]
-  const searchFood = req.body.food[0];
+
 
   // Path finding
   buf = pathfinding.bytesFrom2DArray(width, height, gameboard);
   grid = pathfinding.buildGrid(width, height, buf);
-  path = pathfinding.findPath(headX, headY, searchFood[0], searchFood[1], grid);
+
+  // Food search
+  const pathArray = food.map(foodCords => {
+    return pathfinding.findPath(headX, headY, foodCords[0], foodCords[1], grid);
+  })
+
+  let bestLength = 100;
+  const path = pathArray.reduce((bestPath, pathElement) => {
+    if (pathElement.length < bestLength) {
+      bestPath = pathElement;
+      bestLength = pathElement.length;
+    }
+    return bestPath
+  });
 
   // Convert to int
   const predictX = UINT32(path[1])._high
   const predictY = UINT32(path[1])._low
 
   //console.log(headX, headY, predictX, predictY);
-
   console.log("path on grid :" + (grid.toString(1 << 16 | 0, 1 << 16 | 4, path)));
   //console.log(gameboard);
-
-
-
-  //const nextMove = nextMove(headX, headY, predictX, predictY);
 
   // Response data
   var data = {
     move: nextMove(headX, headY, predictX, predictY), // one of: ['up','down','left','right']
-    taunt: 'Outta my way, snake!', // optional, but encouraged!
+    taunt: 'Outta my way, schnääääägaaaa!', // optional, but encouraged!
   }
 
   return res.json(data)
